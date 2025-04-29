da3bffec52f28f43a5388eaf5f927575f5491892
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2025-04-29 20:36:25 +0700

    adj

--- controllers/CompanyController.go
+++ controllers/CompanyController.go
@@ -31,5 +31,52 @@ func GetCompany (c *fiber.Ctx) error{
 		return c.Status(500).JSON(fiber.Map{"message": "Gagal mengambil data"})
 	}
 		return c.JSON(fiber.Map{"data":company})
+}
 
+func UpdateCompany (c *fiber.Ctx) error{
+	var company models.Company
+	id := c.Params("id")
+
+	if err := databases.DB.Where("id = ?", id).First(&company); err == nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message":"Company tidak ditemukan"})
+	}
+
+
+	var companyUpdate models.Company
+	if err := c.BodyParser(&companyUpdate) ; err != nil{
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	photoUpload, err := c.FormFile("photo")
+	if err == nil{
+		photoPath,err := utils.UploadFile(photoUpload,"photo")
+		if err!= nil{
+			return c.Status(500).JSON(fiber.Map{"message":"Invalid "})
+		}
+		companyUpdate.Photo = photoPath
+	}
+	
+	if  databases.DB.Model(&companyUpdate).Where("id = ?", id).Updates(&companyUpdate).RowsAffected == 0 {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Message":"Gagal mengupdate company "})
+	}
+
+	return  c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+		"message":"Berhasil mengupdate company",
+		"data": companyUpdate,
+	})
+}
+
+func DeleteCompany (c *fiber.Ctx) error {
+	var company models.Company
+	id := c.Params("id")
+
+	if err := databases.DB.Where("id = ?", id).First(&company); err == nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message":"Company tidak ditemukan"})
+	}
+
+	databases.DB.Delete(&company,id)
+	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+		"message:":"Berhasil menghapus company",
+		"data":company,
+	})
 }
--- routes/route.go
+++ routes/route.go
@@ -8,13 +8,16 @@ import (
 
 func SetupRoutes(app *fiber.App){
 	api := app.Group("/api")
+	apiCompany := api.Group("/company")
 	api.Post("/users",controllers.Register)
 	api.Post("/login",controllers.Login)
 	api.Post("/profile",controllers.InsertProfile)
 	api.Get("/profile/:id",controllers.GetProfile)
-	api.Post("/company",controllers.InsertCompany)
-	api.Get("/company",controllers.GetCompany)
 	api.Post("/vacancy",controllers.InsertVacancy)
 	api.Get("/vacancy",controllers.GetVacancy)
+	apiCompany.Post("/",controllers.InsertCompany)
+	apiCompany.Get("/",controllers.GetCompany)
+	apiCompany.Put("/:id",controllers.UpdateCompany)
+	apiCompany.Delete("/:id",controllers.DeleteCompany)
 
 }
