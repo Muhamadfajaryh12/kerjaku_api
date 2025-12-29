cee516655c321a29baf8406237b5b08e25429852
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2025-12-29 19:12:54 +0700

    adj

--- controllers/EducationController.go
+++ controllers/EducationController.go
@@ -20,3 +20,31 @@ func InsertEducation(c *fiber.Ctx) error {
 
 	return c.JSON(input)
 }
+
+func GetEducation(c *fiber.Ctx) error {
+	var education []models.EducationForm
+	
+	if err := databases.DB.Find(&education); err !nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(education)
+}
+
+func DeleteEducation(c *fiber.Ctx) error {
+	var education models.Education
+	id := c.Params("id")
+
+	if err := databases.DB.Where("id = ?",id).First(&education); err != nil {
+		return c.Status(fiber.StatusInternalServerError)
+	}
+
+	if err := databases.DB.Delete(&education, id); err != nil {
+		return c.Status(fiber.StatusInternalServerError)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":"berhasil menghabpus pendidikan",
+		"id":id
+	})
+}
